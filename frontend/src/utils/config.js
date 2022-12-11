ebfffe9d80886635e15bfb6b3b362748ddef8775
vinayak <khandekarvinayak41@gmail.com>
2022-12-11 14:59:34 +0530

    update socket url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 const SOCKET_URL = (function () {
   if (process.env.NODE_ENV === "development") return "http://localhost:8000";
-  return "https://fire-chat01.herokuapp.com";
+  return "https://fire-chat.onrender.com";
 })();
 
 module.exports = { SOCKET_URL };
